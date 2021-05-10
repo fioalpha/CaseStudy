23bd9e2599041eac843cb90e6206fb291095b4a7
fioalpha <fioalpha@gmail.com>
2021-05-09 22:01:33 -0300

    a

--- mergeklint.py
+++ mergeklint.py
@@ -1,29 +1,27 @@
+#!/usr/bin/env bash
+
 import os, os.path, sys
 import glob
 from xml.etree import ElementTree
 
-BASE_DIRECTORY = "./"
+BASE_DIRECTORY = "/bitrise/src"
 
 xml_files = [
-    f"{BASE_DIRECTORY}/app/build/reports/ktlint/ktlint-checkstyle.xml",
-    f"{BASE_DIRECTORY}/component/build/reports/ktlint/ktlint-checkstyle.xml",
-    f"{BASE_DIRECTORY}/data/build/reports/ktlint/ktlint-checkstyle.xml",
-    f"{BASE_DIRECTORY}/domain/build/reports/ktlint/ktlint-checkstyle.xml",
-    f"{BASE_DIRECTORY}/extensions/build/reports/ktlint/ktlint-checkstyle.xml",
+    f"/bitrise/src/app/build/reports/ktlint/ktlint-checkstyle.xml",
+    # f"{BASE_DIRECTORY}/component/build/reports/ktlint/ktlint-checkstyle.xml",
+    # f"{BASE_DIRECTORY}/data/build/reports/ktlint/ktlint-checkstyle.xml",
+    # f"{BASE_DIRECTORY}/domain/build/reports/ktlint/ktlint-checkstyle.xml",
+    # f"{BASE_DIRECTORY}/extensions/build/reports/ktlint/ktlint-checkstyle.xml",
 
 ]
-
-# print(os.path.dirname(os.path.abspath(__file__)))
 
 xml_element_tree = None
 
 for xml_file in xml_files:
-    print(xml_file)
     if not os.path.exists(xml_file):
-        print("asjhd")
-        continue
+        continue;
     if os.stat(xml_file).st_size == 0:
-        continue
+        continue;
 
     data = ElementTree.parse(xml_file).getroot()
 
@@ -35,8 +33,8 @@ for xml_file in xml_files:
             insertion_point.extend(result)
 
 if xml_element_tree is not None:
-    print(ElementTree.tostring(xml_element_tree))
-    f = open( "/Users/wesleysilva/project/poc/CaseStudy/test.xml", 'w' )
+    # print(ElementTree.tostring(xml_element_tree))
+    f = open( f"{BASE_DIRECTORY}/test.xml", 'w' )
     f.write( "\n" )
     f.write( ElementTree.tostring(xml_element_tree).decode("utf-8") )
     f.close()
