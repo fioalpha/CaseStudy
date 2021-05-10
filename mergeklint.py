50d23d9da9a30b4188c5db5f8082f062b0b98887
fioalpha <fioalpha@gmail.com>
2021-05-09 22:03:23 -0300

    a

--- mergeklint.py
+++ mergeklint.py
@@ -34,7 +34,7 @@ for xml_file in xml_files:
 
 if xml_element_tree is not None:
     # print(ElementTree.tostring(xml_element_tree))
-    f = open( f"{BASE_DIRECTORY}/test.xml", 'w' )
+    f = open( "/bitrise/src/test.xml", 'w' )
     f.write( "\n" )
     f.write( ElementTree.tostring(xml_element_tree).decode("utf-8") )
     f.close()
